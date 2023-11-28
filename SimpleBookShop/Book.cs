568f801f28af540f7d613ca427afb7d76c677a47
Ted Pieplow <ted.pieplow@gmail.com>
2023-11-28 19:16:54 +0100

    First commit

---
+++ SimpleBookShop/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleBookShop;
+
+public class Book
+{
+    public string Title { get; set; } = null!;
+    public string Author { get; set; } = null!;
+    public int? ISBN { get; set; }
+
+    public Book()
+    {
+        Title = "Ted";
+        Author = "Pieplow";
+        ISBN = 123;
+    }
+}
---
+++ SimpleBookShop/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SimpleBookShop;
+
+public class Program
+{
+    static List<Book> bookList = [];
+
+    static void Main()
+    {
+        while (true)
+        {
+
+            Console.WriteLine("Välkommen till min bokhanterare");
+            string[] menuOptions = ["1. Add a book", "2. Find a book", "3. Show all books", "0. Exit application"];
+
+            string choice = Console.ReadLine()!;
+
+            switch (choice)
+            {
+                case "1":
+                    AddBook();
+                    break;
+                case "2":
+                    ViewSingleBook();
+                    break;
+            }   
+        }
+    }
+}
